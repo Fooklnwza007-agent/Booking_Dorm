02a725321e6b72c0ea127dc387da91d0a9abc16b
Fooklnwza007-agent <Jeerapat.phum@ku.th>
2025-09-26 22:41:08 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+import UI.*;
+
+public class App{
+    public static void main(String[] args) {
+        new Register();
+        new Login();
+        new Admin();
+        new select();
+    }
+}
