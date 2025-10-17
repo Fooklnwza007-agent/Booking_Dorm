e1b706ef6c4569e01020c3abe275f740fc3135e3
Teelol55 <theethawat.ar@ku.th>
2025-10-17 07:03:37 +0700

    Update App.java

--- App.java
+++ App.java
@@ -1,4 +1,7 @@
-
+import UI.Userinfo;
+import UI.select;
+import UI.Login;
+import UI.Register;
 import UI.User2;
 
 
@@ -6,7 +9,9 @@ public class App{
     public static void main(String[] args) {
         //new Register();
         //new Login();
-        new User2();
+        //new User2();
+        Userinfo userinfo = new Userinfo();
+        userinfo.setVisible(true);
         //new select();
     }
 }
